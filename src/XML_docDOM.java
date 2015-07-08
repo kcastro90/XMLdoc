98ca12652910ec2154171eb7d364d92137ca9659
kcastro90 <kcastro@student.bridgew.edu>
2015-07-07 23:46:42 -0400

    Update XML_docDOM.java

--- src/XML_docDOM.java
+++ src/XML_docDOM.java
@@ -2,45 +2,49 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Scanner;
 
-//import javax.xml.parsers.DocumentBuilder;
-//import javax.xml.parsers.DocumentBuilderFactory;
-
-//import org.w3c.dom.Document;
+/*import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;*/
 
 public class XML_docDOM
-{    
-	public static void main(String[] args) throws Exception
-	{
-		//establish connection with the site
-		URL url = new URL("http://forecast.weather.gov/MapClick.php?lat=42.0418&lon=-70.6723&unit=0&lg=english&FcstType=dwml");
-		HttpURLConnection requestPG = (HttpURLConnection) url.openConnection();
-		requestPG.setRequestMethod("GET");
-		requestPG.connect();
-		
-		//BufferedReader reads from url
-		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(requestPG.getInputStream()));
-		//String line;
-		
-	
-		//scan xml and place it into new file
-		File file = new File("weather.xml");
-		FileWriter write2file = new FileWriter(file);
-		BufferedWriter out  = new BufferedWriter(write2file);
-        
-		//read lines from site
-		while ((bufferedReader.readLine()) != null) 
-		{
-			out.write(bufferedReader.readLine());
-			//System.out.print(line +  "\n");
-		}
-		out.close();
-		
-		//Document doc = docBuilder.parse(XmlFile);
+{
+    public static void main(String[] args) throws Exception
+    {
+        try{
+            //Establish connection with the website
+            URL url = new URL("http://forecast.weather.gov/MapClick.php?lat=42.0418&lon=-70.6723&unit=0&lg=english&FcstType=dwml");
+            HttpURLConnection requestPG = (HttpURLConnection) url.openConnection();
+            requestPG.setRequestMethod("GET");
+            requestPG.connect();
 
-	}
+            //BufferedReader reads from given URL
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(requestPG.getInputStream()));
+            //String line;
+
+
+            //Create a file, BufferedWriter and FileWriter to write to file
+            File file = new File("weather.xml");
+            FileWriter write2file = new FileWriter(file);
+            BufferedWriter out  = new BufferedWriter(write2file);
+
+            //Read lines from website
+            while ((bufferedReader.readLine()) != null)
+            {
+                out.write(bufferedReader.readLine());
+                //out.newLine();
+            }
+            out.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        //Document doc = docBuilder.parse(XmlFile);
+
+    }
 }
